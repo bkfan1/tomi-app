44a94ff6f01b931781564b2433ea8d1f8d994611
bkfan1 <blossomsc15@gmail.com>
2022-10-21 16:02:32 -0400

    Restructured dropdown html

--- src/components/TodoItemActionsMenu.js
+++ src/components/TodoItemActionsMenu.js
@@ -12,41 +12,42 @@ export default function TodoItemActionsMenu({
 
   return (
     <>
-      <ul>
-        <li className="relative">
+      <ul className="relative">
+        <li>
           <button onClick={() => setDisplayOptions(!displayOptions)}>
-            <i className="bi bi-three-dots text-pasto" />
+            <i className={`bi bi-three-dots text-pasto hover:opacity-70 `} />
           </button>
-          {displayOptions ? (
-            <>
-              <ul className="absolute left-[-55px] flex flex-col gap-2 w-24 p-2 border rounded bg-white">
-                <li>
-                  <button
-                    onClick={() => handleDeleteTodo(id)}
-                    className="text-red-500"
-                  >
-                    <i className="bi bi-trash" />
-                    Delete
-                  </button>
-                </li>
-                <li>
-                  <button
-                    onClick={() => setEditing(!editing)}
-                    disabled={editing ? true : false}
-                    className={`${
-                      editing ? "opacity-70 text-gray-500" : "text-blue-500"
-                    } `}
-                  >
-                    <i className="bi bi-pencil" />
-                    Edit
-                  </button>
-                </li>
-              </ul>
-            </>
-          ) : (
-            ""
-          )}
         </li>
+
+        {displayOptions ? (
+          <li>
+            <ul className="absolute left-[-55px] flex flex-col gap-2 w-24 p-2 border rounded bg-white">
+              <li>
+                <button
+                  onClick={() => setEditing(!editing)}
+                  disabled={editing ? true : false}
+                  className={`${
+                    editing ? "opacity-70 text-gray-500" : "text-blue-500"
+                  } `}
+                >
+                  <i className="bi bi-pencil" />
+                  Edit
+                </button>
+              </li>
+              <li>
+                <button
+                  onClick={() => handleDeleteTodo(id)}
+                  className="text-red-500"
+                >
+                  <i className="bi bi-trash" />
+                  Delete
+                </button>
+              </li>
+            </ul>
+          </li>
+        ) : (
+          ""
+        )}
       </ul>
     </>
   );
